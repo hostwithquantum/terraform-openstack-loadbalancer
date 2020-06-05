926d94fe5cb649c278856a691ff85a13f83e6a5b
Thobias Salazar Trevisan <thobias_trevisan@sicredi.com.br>
2020-06-05 17:59:18 -0300

    Add multiple listeners LB - no TERMINATED_HTTPS

--- output.tf
+++ output.tf
@@ -20,35 +20,54 @@ output "loadbalancer_name" {
 
 output "listener_id" {
   description = "Listener ID"
-  value       = openstack_lb_listener_v2.listener.id
+  value = {
+    for i in openstack_lb_listener_v2.listener :
+    i.name => i.id
+  }
 }
 
 output "listener_protocol" {
   description = "Listener protocol"
-  value       = openstack_lb_listener_v2.listener.protocol
+  value = {
+    for i in openstack_lb_listener_v2.listener :
+    i.name => i.protocol
+  }
 }
 
 output "listener_protocol_port" {
   description = "The port on which to listen for client traffic"
-  value       = openstack_lb_listener_v2.listener.protocol_port
+  value = {
+    for i in openstack_lb_listener_v2.listener :
+    i.name => i.protocol_port
+  }
 }
 
 output "lb_pool_id" {
   description = "Load balancer pool ID"
-  value       = openstack_lb_pool_v2.lb_pool.id
-}
-
-output "member_port" {
-  description = "The port on which to listen for client traffic"
-  value = join(",",
-    distinct([for member in openstack_lb_member_v2.member : member.protocol_port])
-  )
+  value = {
+    for i in openstack_lb_pool_v2.lb_pool :
+    i.name => i.id
+  }
 }
 
 output "member_address" {
   description = "The IP address of the member to receive traffic from the load balancer"
-  value = join(",",
-    [for member in openstack_lb_member_v2.member :
-    member.address]
-  )
+  value = {
+    for i in openstack_lb_members_v2.members :
+    i.pool_id => i.member
+  }
 }
+#output "member_port" {
+#  description = "The port on which to listen for client traffic"
+#  value = join(",",
+#    distinct([for member in openstack_lb_member_v2.member : member.protocol_port])
+#  )
+#}
+#
+#output "member_address" {
+#  description = "The IP address of the member to receive traffic from the load balancer"
+#  value = join(",",
+#    [for member in openstack_lb_member_v2.member :
+#    member.address]
+#  )
+#}
